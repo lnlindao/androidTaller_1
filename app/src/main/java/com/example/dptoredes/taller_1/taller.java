ffe1c0a9d7ca8cbfc628dd58284480fee4a94e4f
lnlindao <lnlindao@hotmail.com>
2017-06-01 10:44:57 -0500

    first commit

---
+++ app/src/main/java/com/example/dptoredes/taller_1/taller.java
@@ -0,0 +1,13 @@
+package com.example.dptoredes.taller_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class taller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taller);
+    }
+}
